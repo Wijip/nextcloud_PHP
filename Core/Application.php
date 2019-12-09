ae8a3bf0045226420095923f8ca4646b5021bd7c
Wijip <wiji.prabowo0857@gmail.com>
2019-12-09 09:54:47 +0700

    menambah Base dan check

---
+++ Core/Application.php
@@ -0,0 +1,8 @@
+<?php
+namespace OC\Core;
+
+use OC\Core\Notification\RemoveLinkSharesNotifier;
+use OC\DB\MissingIndexInformation;
+use OC\DB\SchemaWrapper;
+use OCP\AppFramework\App;
+use OCP\IDBConnection;
